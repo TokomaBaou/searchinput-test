254f1a4408151996a8e4d1999867593301e449d1
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-06-08 23:57:44 +0900

    関数コンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./styles.css";
+import type { VFC } from "react";
 
-export default function App() {
+export const App: VFC = () => {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
-}
+};
+
+// propsに直接型注釈を指定するパターン
+// React.VFC<P>のジェネリック型<P>として型を指定するパターン
+// どちらのパターンで書いても結果は変わりません。
